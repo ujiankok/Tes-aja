0a1bc19a06dbafa597fa6c99a17c3fdf1ed9710c
ujiankok <tejobilar7@gmail.com>
2024-06-01 11:37:05 +0700

    Delete tests/utilities directory

--- tests/utilities/test_encoding.py
+++
@@ -1,9 +0,0 @@
-from bot.utilities.helpers import Encoding
-
-
-def test_encoding() -> None:
-    file_ids = [1, 2, 3, 4, 5]
-    encoded_string = Encoding.encode(file_ids)
-    assert encoded_string == "WzEsIDIsIDMsIDQsIDVd"
-    decoded_ids = Encoding.decode(encoded_string)
-    assert decoded_ids == file_ids
