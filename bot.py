29ee19a67afdead0885eb62fe153178697a92e2a
ujiankok <tejobilar7@gmail.com>
2024-06-01 23:53:21 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from aiohttp import web
 from pyrogram import Client
 from pyrogram.enums import ParseMode
 from datetime import datetime
-from bot.import NoInviteLinkError, PyroHelper
+from bot import PyroHelper
 
 from config import (
     API_HASH,
